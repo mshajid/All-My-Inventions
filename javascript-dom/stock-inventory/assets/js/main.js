ddbb933d32329bdbef386d048a78d6f90e3c753e
Shajid <shajid1biz@gmail.com>
2023-12-23 14:07:31 +0530

    feat: errorCSS labels for input fields

--- javascript-dom/stock-inventory/assets/js/main.js
+++ javascript-dom/stock-inventory/assets/js/main.js
@@ -5,32 +5,48 @@ const inventoryEl = document.getElementById("stockInventory");
 const validate = new JustValidate("#stockInventory");
 console.log(validate);
 
-validate.addField("#productId", [
-  {
-    rule: "required",
-  },
-]);
+validate.addField(
+  "#productId",
+  [
+    {
+      rule: "required",
+    },
+  ],
+  { errorLabelCssClass: ["form-error"] }
+);
 
-validate.addField("#productName", [
-  {
-    rule: "required",
-  },
-]);
+validate.addField(
+  "#productName",
+  [
+    {
+      rule: "required",
+    },
+  ],
+  { errorLabelCssClass: ["form-error"] }
+);
 
-validate.addField("#quantities", [
-  {
-    rule: "required",
-  },
-]);
+validate.addField(
+  "#quantities",
+  [
+    {
+      rule: "required",
+    },
+  ],
+  { errorLabelCssClass: ["form-error"] }
+);
 
-validate.addField("#supervisor", [
-  {
-    rule: "required",
-  },
-]);
+validate.addField(
+  "#supervisor",
+  [
+    {
+      rule: "required",
+    },
+  ],
+  { errorLabelCssClass: ["form-error"] }
+);
 
 validate.addField("#dateEntry", [
   {
     rule: "required",
   },
-]);
+], { errorLabelCssClass: ["form-error"] });
